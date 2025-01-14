59302e11f861e383a877b1ef72d2dad0f4d6b02d
ethan <ethanjriley02@gmail.com>
2025-01-14 11:36:43 +0000

    Adds player routers

--- controllers/players.controller.js
+++ controllers/players.controller.js
@@ -1,7 +1,21 @@
-const { fetchPlayers } = require("../models/players.models");
+const { fetchPlayers, insertPlayer } = require("../models/players.models");
 
 exports.getPlayers = (req, res, next) => {
-  fetchPlayers().then((rows) => {
-    res.status(200).send({ players: rows });
-  });
+  fetchPlayers()
+    .then((players) => {
+      res.status(200).send({ players });
+    })
+    .catch(next);
+};
+
+exports.postPlayer = (req, res, next) => {
+  const {
+    body: { player_name },
+  } = req;
+
+  insertPlayer(player_name)
+    .then((player) => {
+      res.status(201).send({ player });
+    })
+    .catch(next);
 };
--- models/players.models.js
+++ models/players.models.js
@@ -7,3 +7,13 @@ exports.fetchPlayers = () => {
     return rows;
   });
 };
+
+exports.insertPlayer = (player_name) => {
+  return db
+    .query("INSERT INTO players (player_name) VALUES ($1) RETURNING *;", [
+      player_name,
+    ])
+    .then(({ rows }) => {
+      return rows[0];
+    });
+};
