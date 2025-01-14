951ec14b7024ec0b1c233b9a15e24844142f6a67
ethan <ethanjriley02@gmail.com>
2025-01-14 10:32:24 +0000

    Adds routes

--- routes/api.router.js
+++ routes/api.router.js
@@ -1,2 +1,16 @@
 const express = require("express");
+const playerRouter = require("./players.router");
+const classesRouter = require("./classes.router");
+const levelsRouter = require("./levels.router");
+const leaderboardRouter = require("./leaderboard.router");
+const { getEndpoints } = require("../controllers/api.controllers");
+
 const apiRouter = express.Router();
+
+apiRouter.get("/", getEndpoints);
+apiRouter.get("/players", playerRouter);
+apiRouter.get("/classes", classesRouter);
+apiRouter.get("/levels", levelsRouter);
+apiRouter.get("/leaderboard", leaderboardRouter);
+
+module.exports = apiRouter;
