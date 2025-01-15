d9382da2e1d5a29703439d0e5dba9bdcb742b9c9
ethan <ethanjriley02@gmail.com>
2025-01-15 12:35:09 +0000

    Adds new user credentials

--- models/leaderboard.models.js
+++ models/leaderboard.models.js
@@ -3,8 +3,8 @@ const db = require("../db/connection");
 exports.selectLeaderboard = (level_id) => {
   let sqlQuery = `SELECT leaderboard.*, players.player_name, levels.level_name, classes.class_name
     FROM leaderboard
-    JOIN players on leaderboard.player_id = player.player_id
-    JOIN levels on leaderboard.level_id = levels.level_id
+    JOIN players ON leaderboard.player_id = players.player_id
+    JOIN levels ON leaderboard.level_id = levels.level_id
     JOIN classes ON leaderboard.class_id = classes.class_id`;
 
   const params = [];
@@ -13,7 +13,7 @@ exports.selectLeaderboard = (level_id) => {
     sqlQuery += ` WHERE leaderboard.level_id = $1`;
   }
 
-  query += ` ORDER BY score DESC;`;
+  sqlQuery += ` ORDER BY score DESC;`;
   return db.query(sqlQuery, params).then(({ rows }) => {
     return rows;
   });
@@ -32,7 +32,7 @@ exports.insertLeaderboardEntry = (
         VALUES ($1, $2, $3, $4, $5) RETURNING *;`,
       [player_id, level_id, class_id, score, completion_time]
     )
-    .then((rows) => {
+    .then(({ rows }) => {
       return rows[0];
     });
 };
