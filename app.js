b13df8fd6bbbcd8f1a7a61cb05535e4aa1a78bc3
ethan <ethanjriley02@gmail.com>
2025-01-21 10:00:18 +0000

    Removed listening from app.js

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const apiRouter = require("./routes/api.router");
 
-const cors = require('cors');
-const passport = require('passport');
-const dotenv = require('dotenv');
-const playersRouter = require('./routes/players.router.js')
+const cors = require("cors");
+const passport = require("passport");
+const dotenv = require("dotenv");
+const playersRouter = require("./routes/players.router.js");
 
-dotenv.config(); 
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(passport.initialize());
-require('./db/passport')(passport);
+require("./db/passport")(passport);
 
-app.use('/', playersRouter)
+app.use("/", playersRouter);
 
 app.use("/api", apiRouter);
 
@@ -36,8 +36,5 @@ app.use((err, req, res, next) => {
   }
   res.status(500).send({ msg: "Internal server error" });
 });
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
 
 module.exports = app;
